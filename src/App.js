9b038a3aaed0cc6211d35ca539ed7aa0396de418
haichungcn <haichungcn@gmail.com>
2019-10-16 10:10:45 +0700

    final

--- src/App.js
+++ src/App.js
@@ -6,19 +6,18 @@ import Main from './components/Main';
 
 function SignInState(props) {
   return (
-  <div className="form-group username">
-    <div className="form-group">
-      <i className="fas fa-user"></i>
-      <input type="text" className="form-control" placeholder="Username" required></input>
-      <i className="fas fa-arrow-right"></i>
+    <div className="container d-flex justify-content-center align-items-center">
+      <div className="form-group username">
+        <div className="form-group">
+          <i className="fas fa-user"></i>
+          <input type="text" className="form-control" placeholder="Username" required></input>
+        </div>
+        <div className="form-group password">
+          <input type="password" className="form-control" placeholder="Username" required></input>
+        </div>
+        <button onClick={() => props.onLoggedIn(true)}>Sign In</button>
+      </div>
     </div>
-    <div className="form-group password">
-      <i className="fas fa-user"></i>
-      <input type="password" className="form-control" placeholder="Username" required></input>
-      <i className="fas fa-arrow-right"></i>
-    </div>
-    <button onClick={() => props.onLoggedIn(true)}>Sign In</button>
-  </div>
   );
 }
 
